4724eb065a4952c54e58a3970656b5d1dd292ff7
github username <github email address>
2018-08-11 18:20:34 -0400

    Defined method using for loop.

--- for.rb
+++ for.rb
@@ -1,7 +1,13 @@
 
-def using_for
+def using_for(items = [1])
 	checklist = 1..10
 	#your code here
+	index = 0
+	while index < items.length do
+		index = index + 1
+		counter = 1
+		for counter in checklist do
+			puts "Wingardium Leviosa"
+		end
+	end
 end
-
-
